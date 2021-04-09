770c3b98a2767cf0ff02604278b6edd3a6f5f0b2
Ar Kar <arkar20011@gmail.com>
2021-04-09 18:44:49 +0630

    livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        @@livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-            <example-component></example-component>
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -33,4 +33,5 @@
             </main>
         </div>
     </body>
+    @livewireScripts
 </html>
