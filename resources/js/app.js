40bfe02d9ae987bf161ea7c8d536117edc7f05a9
Ar Kar <arkar20011@gmail.com>
2021-04-09 18:15:43 +0630

    livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 require('./bootstrap');
 
 require('alpinejs');
+window.Vue = require('vue').default;
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
